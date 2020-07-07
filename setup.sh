468cb13dad2d2c5e8ec44d9d7fe739ba33aaa4a3
lenovix <ichkmlsdr@gmail.com>
2020-07-07 21:34:27 +0700

    modify file setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ sudo apt install python3.8
 # to install pip3
 sudo apt-get install python3-pip
 # to install instaloader
-pip install instaloader
+pip3 install instaloader
 # to update
 sudo apt update
