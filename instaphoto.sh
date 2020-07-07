8791775cb3f181ce41b020749e792c499d9bf555
lenovix <ichkmlsdr@gmail.com>
2020-07-08 01:38:49 +0700

    add and modify code in the file

--- instaphoto.sh
+++ instaphoto.sh
@@ -1,8 +1,11 @@
 read -p "Username target(ex:lenovix123): @" Uname
 echo Connecting...
 instaloader profile $Uname
+rm $Uname/*.txt
+rm $Uname/*.json.xz
 clear
 echo '========================================'
 echo Photo @$Uname Complete to Download
 echo And save in folder $Uname
 echo '========================================'
+tree $Uname/
--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ sudo apt install python3.8
 sudo apt-get install python3-pip
 # to install instaloader
 pip3 install instaloader
+# to install tree
+sudo apt install tree
 # to update
 sudo apt update
