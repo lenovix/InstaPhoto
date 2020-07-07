d6480473039178d60293b34153821716d9eff60c
lenovix <ichkmlsdr@gmail.com>
2020-07-07 20:08:12 +0700

    this is first commit for this repo

---
+++ instaphoto.sh
@@ -0,0 +1,8 @@
+read -p "Username target(ex:lenovix123): @" Uname
+echo Please Wait...
+instaloader profile $Uname
+clear
+echo '========================================'
+echo Photo @$Uname Complete to Download
+echo And save in folder $Uname
+echo '========================================'
