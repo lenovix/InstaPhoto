7592fe3ebf0166778870d49e57df3dc5375f5115
lenovix <ichkmlsdr@gmail.com>
2020-07-10 00:15:54 +0700

    edit command

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt install python3.8
 # to install pip3
 sudo apt-get install python3-pip
 # to install instaloader
-pip3 install instaloader
+sudo pip3 install instaloader
 # to install tree
 sudo apt install tree
 # to update
